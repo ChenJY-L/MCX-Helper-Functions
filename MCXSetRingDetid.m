16c3007807b408b53dc8bf542b61671cc607329d
ChenJY-L <843609793@qq.com>
2025-02-25 15:04:52 +0800

    [feat] 添加不同宽度SDS的接口

--- MCXSetRingDetid.m
+++ MCXSetRingDetid.m
@@ -15,9 +15,16 @@ function [detp, idNum] = MCXSetRingDetid(detp, center, SDS, SDSWidth)
 pos = detp.p(:, 1:2) - center;
 distance = sqrt(pos(:, 1).^2 + pos(:, 2).^2);
 
-for i = 1:length(SDS)
-    id = (distance >= (SDS(i) - SDSWidth / 2)) & (distance < (SDS(i) + SDSWidth / 2));
-    detp.detid(id) = i;
+if isscalar(SDSWidth)    % 如果仅设置一个SDS宽度
+    for i = 1:length(SDS)
+        id = (distance >= (SDS(i) - SDSWidth / 2)) & (distance < (SDS(i) + SDSWidth / 2));
+        detp.detid(id) = i;
+    end
+else    % 否则的话，不同SDS具有不同的宽度
+    for i = 1:length(SDS)
+        id = (distance >= (SDS(i) - SDSWidth(i) / 2)) & (distance < (SDS(i) + SDSWidth(i) / 2));
+        detp.detid(id) = i;
+    end
 end
 idNum = length(SDS);
 
